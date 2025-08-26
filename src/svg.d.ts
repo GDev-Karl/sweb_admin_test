411789b9888bea96f29b30fa1bf7c8b7ee1b816c
Karl <tekamkarl@gmail.com>
2025-08-26 09:41:41 +0100

    Refactor: Update SVG module declarations for improved type safety

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,9 +1,13 @@
-/// <reference types="vite/client" />
 
 
 declare module '*.svg' {
   import * as React from 'react';
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
-  const src: string;
-  export default src;
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
+
+declare module '*.svg?react' {
+  import * as React from 'react';
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
 }
